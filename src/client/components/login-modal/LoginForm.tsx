d991b22ea6740fa95ae501ffcb80caa61fdd540c
noFrontendSolutions <b.welzel81@gmail.com>
2022-07-08 00:28:30 +0200

    Refactoring input fields of Login and SignUp into different files.

--- src/client/components/login-modal/LoginForm.tsx
+++ src/client/components/login-modal/LoginForm.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react"
 import { UserContext } from "../../context/UserContext"
+import LoginInputs from "./LoginInputs"
 
 const LoginForm = () => {
   const {
@@ -16,38 +17,11 @@ const LoginForm = () => {
 
   return (
     <form action="/login" className="h-full p-4 pt-2 pl-0 flex flex-col">
-      <label htmlFor="email" className="font-bold">
-        Email:
-      </label>
-      <input
-        type="text"
-        name="email"
-        placeholder="Email..."
-        value={credentials?.email ? credentials?.email : ""}
-        className="rounded w-full mb-4 mt-2 py-2 px-3 ring-2 ring-gray-400 focus:outline-none focus:ring-4 focus:ring-blue-500"
-        onChange={(e) =>
-          setCredentials({ ...credentials, email: e.target.value })
-        }
+      <LoginInputs
+        credentials={credentials}
+        setCredentials={setCredentials}
+        error={error}
       />
-      {error?.email && (
-        <p className="mb-2 text-red-500 font-bold">{error?.email}</p>
-      )}
-      <label htmlFor="password" className="font-bold">
-        Password:
-      </label>
-      <input
-        type="password"
-        name="first-name"
-        placeholder="Password..."
-        value={credentials?.password ? credentials?.password : ""}
-        className="rounded w-full mb-4 mt-2 py-2 px-3 ring-2 ring-gray-400 focus:outline-none focus:ring-4 focus:ring-blue-500"
-        onChange={(e) =>
-          setCredentials({ ...credentials, password: e.target.value })
-        }
-      />
-      {error?.password && (
-        <p className="mb-2 text-red-500 font-bold">{error?.password}</p>
-      )}
       <button
         type="submit"
         className="bg-blue-600 rounded-lg hover:bg-blue-700 font-bold text-xl text-white h-14 mt-8 flex justify-center items-center"
